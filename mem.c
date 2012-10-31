f7f06aa80b55ddb1425c48b29a4fa8534ae296fa
Joseph Moster <jemoster@gmail.com>
2012-10-31 18:58:31 -0400

    fixed getMem to return correct pointer

--- mem.c
+++ mem.c
@@ -9,13 +9,12 @@ void* getMem(uint32_t size) {
 	static uint32_t* start_data = &_sheap;
 	uint32_t* end_data = &_eheap;
     
-    uint32_t* start_addr;
+    uint32_t* start_addr = start_data;
     if(start_data+size<end_data){
-        start_addr = (uint32_t*)((uint32_t)start_data+size);
-        start_data = start_addr;
+        start_data = (uint32_t*)((uint32_t)start_data+size);
     }
     else {
         start_addr = NULL;
     }
-    return start_data;
+    return start_addr;
 }
